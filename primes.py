fa68d04a8e5fb0096f528ced0b3736715542f607
Saskia Lablans <k21009051@kcl.ac.uk>
2022-09-28 09:52:10 +0100

    Add exception for <= 0 argument

--- primes.py
+++ primes.py
@@ -1,6 +1,22 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"{number_of_primes} needs to be a positive number!")
+
     list = []
+    testing_number = 2
+
+    while len(list) < number_of_primes:
+        prime = True
+
+        for i in range(2,(testing_number//2)+1):
+            if testing_number % 1 == 0:
+                prime = False
+                break
+
+        if prime == True:
+            list.append(testing_number)
+        testing_number += 1
+
     return list
